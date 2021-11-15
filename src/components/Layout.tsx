524dd149d061c44a9c4450901f616de029484aee
Ari Perez <ariperez@users.noreply.github.com>
2021-11-14 20:37:06 -0500

    feat: integrate sb components (#67)
    * feat: integrate sb components
    
    * ci: publish to gatsby cloud
    
    * fix: use workign super litner
    
    * Rename layout.tsx to Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
-import { Header } from "./Header";
+import { Header } from "@graydigital/storybook";
+import logo from "../images/gray_digital_logo_white.png";
+
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <Header />
+      <Header
+        LogoImage={() => (
+          <img width={107} height={30} src={logo} alt="gray logo" />
+        )}
+        Link={() => <a href="/careers">Join Us</a>}
+        asLogoLink={null}
+      />
       <main>{children}</main>
     </>
   );
