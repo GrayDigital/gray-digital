62bd2520f858b2415b75b45aa6d74be252d20be4
Ari P <ariperez@users.noreply.github.com>
2021-11-19 15:57:31 -0500

    fix: gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,5 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
   ],
 };
