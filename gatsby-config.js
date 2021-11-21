cc223633084880a4c86df27245ed49c75fad6267
Ari P <ariperez@users.noreply.github.com>
2021-11-20 20:37:00 -0500

    feat: contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config({
+  // path: `.env.${process.env.NODE_ENV}`,
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gray Digital`,
@@ -14,6 +19,13 @@ module.exports = {
          * if `false`, this plugin will not use `<CSSReset />
          */
         resetCSS: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `ap1mxri861el`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     "gatsby-plugin-image",
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,36 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-
 import { Header } from "@graydigital/storybook";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 export const Layout: React.FC = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    {
+      contentfulHeaderMenu(
+        title: {}
+        contentful_id: { eq: "HAtlPdiuqVPAag5Rg8bOs" }
+      ) {
+        title
+        logoAlt
+        logo {
+          gatsbyImageData(width: 107, height: 30)
+        }
+      }
+    }
+  `);
+
+  const image = getImage(data.contentfulHeaderMenu.logo);
+
   return (
     <>
       <Header
-        LogoImage={() => <img width={107} height={30} alt="gray logo" />}
+        LogoImage={() => (
+          <GatsbyImage image={image} alt={data.contentfulHeaderMenu.logoAlt} />
+        )}
         Link={() => <a href="/careers">Join Us</a>}
         asLogoLink={null}
       />
       <main>{children}</main>
     </>
   );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 };
